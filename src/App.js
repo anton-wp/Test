bc82cbfa6355e6e05db131915a947ae78e2766ae
Gorbenko <anton69917091@gmail.com>
2020-02-21 14:31:34 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-// import { Provider } from 'react-redux';
-// import store from './store';
 import Game from './components/game'
-// import Header from './components/header'
+
 
 function App() {
   
 
   return (
     <div className="App">
-      {/* <Provider store={store}> */}
-        <header >
-          {/* <Header /> */}
-          <Game />
-        </header>
-      {/* </Provider> */}
+      <header >
+        <Game />
+      </header>
     </div>
   );
 }
